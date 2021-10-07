88394e34e2e50e6563de8649d732297f0b0cd517
stevenjhomem <sjhomem@outlook.com>
2021-10-07 16:12:02 -0500

    break

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,16 +1,21 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Recipe = require("./recipes-middleware");
 
-router.use('*', (req,res)=>{
-    res.json({api: "is up and running!"})
-})
+router.get("/:recipe_id", (req, res, next) => {
+  Recipe.getRecipeById(req.params.recipe_id)
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
 
 // eslint-disable-next-line no-unused-vars
-router.use((err,req,res,next)=>{ 
-    res.status(500).json({
-        CustomMessage: "Something went wrong in the recipes database",
-        Message: err.message,
-        stack: err.stack
-    })
-})
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    CustomMessage: "Something went wrong in the recipes database",
+    Message: err.message,
+    stack: err.stack,
+  });
+});
 
-module.exports = router
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require("express").Router();
 const recipesRouter = require("../api/recipes/recipes-router");
 
 const server = express();
@@ -6,5 +7,9 @@ const server = express();
 server.use(express.json());
 
 server.use("/api/recipes", recipesRouter);
+
+router.use("*", (req, res) => {
+    res.json({ api: "is up and running!" });
+  });
 
 module.exports = server;
